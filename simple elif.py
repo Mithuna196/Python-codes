a928431c624a5c952ec55ba75e771cfa5dfdeabf
Mithuna Gollapalli <125589683+Mithuna196@users.noreply.github.com>
2024-07-09 22:50:59 +0530

    Create simple elif.py
    𝐞𝐥𝐢𝐟:- 𝐈𝐧 𝐏𝐲𝐭𝐡𝐨𝐧, 𝐭𝐡𝐞 𝐞𝐥𝐢𝐟 𝐬𝐭𝐚𝐭𝐞𝐦𝐞𝐧𝐭 𝐢𝐬 𝐮𝐬𝐞𝐝 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 𝐦𝐮𝐥𝐭𝐢𝐩𝐥𝐞 𝐜𝐨𝐧𝐝𝐢𝐭𝐢𝐨𝐧𝐬 𝐢𝐧 𝐚𝐧 𝐢𝐟-𝐞𝐥𝐢𝐟-𝐞𝐥𝐬𝐞 𝐜𝐡𝐚𝐢𝐧. 𝐈𝐭 𝐬𝐭𝐚𝐧𝐝𝐬 𝐟𝐨𝐫 "𝐞𝐥𝐬𝐞 𝐢𝐟" 𝐚𝐧𝐝 𝐚𝐥𝐥𝐨𝐰𝐬 𝐲𝐨𝐮 𝐭𝐨 𝐭𝐞𝐬𝐭 𝐚𝐝𝐝𝐢𝐭𝐢𝐨𝐧𝐚𝐥 𝐜𝐨𝐧𝐝𝐢𝐭𝐢𝐨𝐧𝐬 𝐢𝐟 𝐭𝐡𝐞 𝐩𝐫𝐞𝐯𝐢𝐨𝐮𝐬 𝐢𝐟 𝐨𝐫 𝐞𝐥𝐢𝐟 𝐜𝐨𝐧𝐝𝐢𝐭𝐢𝐨𝐧𝐬 𝐚𝐫𝐞 𝐧𝐨𝐭 𝐦𝐞𝐭.
    
    𝐇𝐞𝐫𝐞'𝐬 𝐡𝐨𝐰 𝐢𝐭 𝐰𝐨𝐫𝐤𝐬:  
    >>𝐢𝐟 𝐬𝐭𝐚𝐭𝐞𝐦𝐞𝐧𝐭: 𝐂𝐡𝐞𝐜𝐤 𝐭𝐡𝐞 𝐟𝐢𝐫𝐬𝐭 𝐜𝐨𝐧𝐝𝐢𝐭𝐢𝐨𝐧. 
    >>𝐞𝐥𝐢𝐟 𝐬𝐭𝐚𝐭𝐞𝐦𝐞𝐧𝐭: 𝐈𝐟 𝐭𝐡𝐞 𝐟𝐢𝐫𝐬𝐭 𝐜𝐨𝐧𝐝𝐢𝐭𝐢𝐨𝐧 𝐢𝐬 𝐧𝐨𝐭 𝐭𝐫𝐮𝐞, 𝐜𝐡𝐞𝐜𝐤 𝐚𝐧𝐨𝐭𝐡𝐞𝐫 𝐜𝐨𝐧𝐝𝐢𝐭𝐢𝐨𝐧. 
    >>𝐞𝐥𝐬𝐞 𝐬𝐭𝐚𝐭𝐞𝐦𝐞𝐧𝐭: 𝐈𝐟 𝐧𝐨𝐧𝐞 𝐨𝐟 𝐭𝐡𝐞 𝐜𝐨𝐧𝐝𝐢𝐭𝐢𝐨𝐧𝐬 𝐚𝐫𝐞 𝐭𝐫𝐮𝐞, 𝐝𝐨 𝐬𝐨𝐦𝐞𝐭𝐡𝐢𝐧𝐠 𝐞𝐥𝐬𝐞.
    Student Login Script
    
    Description
    
    This Python script simulates a simple login system for students. It prompts the user to enter a username and password and checks the credentials against predefined pairs. Based on the entered credentials, it displays a personalized welcome message or an access denial message.
    
    1.How the code Works
    Prompt for Login:
    
    The script starts by displaying "Student Login:".
    It then prompts the user to enter their username and password.
    
    2.Check Credentials:
    
    The script checks the entered username and password against the following predefined pairs:
    Username: Mona, Password: Manu
    Username: Manasa, Password: Mannu
    Username: mohan, Password: Man
    Depending on the entered credentials, it prints one of the following messages:
    "Welcome Mona!" if the username is Mona and the password is Manu.
    "Hello Manu! Welcome to the world of coding!" if the username is Manasa and the password is Mannu.
    "Hello Manmohan! Welcome to the office as a new manager." if the username is mohan and the password is Man.
    "Sorry Dude!! You're not allowed to enter the office." if none of the above conditions are met.

---
+++ simple elif.py
@@ -0,0 +1,11 @@
+print("Student Login:")
+username = input("username >")
+password = input("password >")
+if (username == "Mona" and password == "Manu"):
+  print("Welcome Mona!")
+elif (username == "Manasa" and password == "Mannu"):
+  print("Hello Manu!.Welcome to the world of coding!")
+elif (username =="mohan" and password =="Man"):
+  print("Hello Mohan!.Welocme to the office as a new manager.")
+else:
+  print("Sorry Dude!! Youre not allowed to enter the office.")
